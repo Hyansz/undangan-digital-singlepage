c7e4a376a3b4dc4023a82daea31e6ee4ab19df33
Raihan <raihanregita@gmail.com>
2025-08-29 08:54:34 +0700

    menambahkan waktu

--- src/components/SaveTheDate.jsx
+++ src/components/SaveTheDate.jsx
@@ -3,6 +3,16 @@ export default function SaveTheDate() {
         <section className="text-center py-12">
             <div className="w-11/12 mx-auto" data-aos="zoom-in">
                 <img src="/img/tanggal.webp" alt="tanggal" className="w-full" />
+                <div className="text-xl font-semibold w-[90%] mx-auto">
+                    <img src="/img/batik-nama1.webp" alt="" />
+                    <div className="py-2">
+                        <p>Waktu :</p>
+                        <p className="text-yellow-400 text-2xl pt-2">
+                            09.00 - 11.30 WIB
+                        </p>
+                    </div>
+                    <img src="/img/batik-nama2.webp" alt="" />
+                </div>
             </div>
         </section>
     );
